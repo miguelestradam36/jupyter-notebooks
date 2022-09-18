953a8381813d125910a5c1b55dc11979853621de
Miguel Estrada <miguelestradam36@estudiantec.cr>
2022-09-18 16:20:06 -0600

    Updating jupyter notebook

--- src/notebooks/sql/example_1.sql
+++ src/notebooks/sql/example_1.sql
@@ -2,5 +2,4 @@ SELECT Stock.Make, COUNT(DISTINCT Stock.Model) AS DIFFERENT_MODELS, COUNT(Stock.
     LEFT JOIN DateDimension ON Stock.PurchaseDate = DateDimension.DateKey
     WHERE DateDimension.QuarterAbbrAndYear IN ('Q1 2015', 'Q3 2015')
     GROUP BY Stock.Make
-    ORDER BY TOTAL_PURCHASES DESC
-    LIMIT 3;
+    ORDER BY TOTAL_PURCHASES DESC;
--- src/notebooks/sql/example_2.sql
+++ src/notebooks/sql/example_2.sql
@@ -1,15 +1,4 @@
-WITH CTE AS (
-        SELECT DateDimension.QuarterAndYear AS "Quarter", 
-            Colors.Color AS "Color", 
-            COUNT(Stock.PurchaseDate) AS "Total Sold"
-		FROM Stock
-        INNER JOIN DateDimension ON Stock.PurchaseDate = DateDimension.DateKey
-        INNER JOIN Colors ON Colors.ColorID = Stock.ColorID
-		GROUP BY "Quarter", "Color"
-		HAVING DateDimension.Year BETWEEN 2012 AND 2015
-    ) SELECT 
-		CTE."Quarter", 
-		CTE."Color", 
-		CTE."Total Sold"
-    FROM CTE
-	ORDER BY "Total Sold" DESC;
+SELECT Stock.Make, COUNT(DISTINCT Stock.Model) AS DIFFERENT_MODELS FROM Stock
+    LEFT JOIN DateDimension ON Stock.PurchaseDate = DateDimension.DateKey
+    GROUP BY Stock.Make
+    ORDER BY TOTAL_PURCHASES DESC;
